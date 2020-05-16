2db3cffebf72de7910d9e30fcdf7876f74a3257f
David Lance <david6116@yahoo.com>
2020-05-16 12:21:18 -0400

    add text box, submit button, and a click handler on the submit button

--- flightstats/app.js
+++ flightstats/app.js
@@ -5,18 +5,22 @@ $(() => {
     //accessing this url each time costs .5second to 1.5seconds,
     //this will be exponetially bad
     //store the result once and resuse it to speed it up.
-    url: "https://opensky-network.org/api/states/all"
+    url: "" //"https://opensky-network.org/api/states/all"
   }).then(
     data => {
-      buildOpenSkyDB(data);
-      calculateflights(openSkyDB);
-      fastestPlane(openSkyDB);
-      highestPlane(openSkyDB);
+      // buildOpenSkyDB(data);
+      // calculateflights(openSkyDB);
+      // fastestPlane(openSkyDB);
+      // highestPlane(openSkyDB);
     },
     error => {
       console.log(error);
     }
   ); //end of ajax
+  $(".btn").on("click", event => {
+    let $value = $("input").val();
+    console.log($value);
+  });
 }); //end of DOM load
 
 //let's make a local copy of the db so that our requests don't exponetially increase load time.
